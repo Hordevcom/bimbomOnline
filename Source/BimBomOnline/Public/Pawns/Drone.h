cf8b7449f30c1d2c19e2ce3f6dc9c2b648d2887b
Viel <vielavernus@gmail.com>
2024-07-20 03:07:26 +0300

    add lab assets and create door actor

---
+++ Source/BimBomOnline/Public/Pawns/Drone.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Drone.generated.h"
+
+UCLASS()
+class BIMBOMONLINE_API ADrone : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ADrone();
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+};
